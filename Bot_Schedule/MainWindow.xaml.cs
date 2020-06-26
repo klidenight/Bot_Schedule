4bf37e84789386b04fa3410ff34f68d6cf688aa5
klidenight <klidenight@mail.ru>
2020-06-26 16:12:07 +1100

    Добавлен тестовый проект

---
+++ Bot_Schedule/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Bot_Schedule
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
